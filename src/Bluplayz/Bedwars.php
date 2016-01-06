6a45314dc5aee48c7fb3ab04469c3575f04956fe
Bluplayz <bluplayz98@gmail.com>
2016-01-06 22:45:49 +0100

    Update Bedwars.php

--- src/Bluplayz/Bedwars.php
+++ src/Bluplayz/Bedwars.php
@@ -7,6 +7,11 @@ use pocketmine\event\Listener;
 
 class Bedwars extends PluginBase implements Listener {
 
+    public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+    }
+
 
 
 }
